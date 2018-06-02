3afc77a198d2cb6f8e16e1e23d3597672f30cc51
LeonardMeagher2 <leonardMeagher2@gmail.com>
2018-06-02 06:00:22 -0700

    fix Uv Width Factor

--- 2D/2DTrail.gd
+++ 2D/2DTrail.gd
@@ -9,8 +9,8 @@ export(Texture) var texture
 export(Texture) var normal_map
 export(Gradient) var color
 
-export(Vector2) var uv_offset = Vector2(0,0)
-export(Vector2) var uv_scale = Vector2(1,1)
+export(Vector2) var uv_offset = Vector2(0.0,0.0)
+export(Vector2) var uv_scale = Vector2(1.0,1.0)
 export(float,-360.0,360.0) var uv_rotation = 0.0
 
 export(float) var width = 50.0
@@ -25,6 +25,7 @@ var points_data = []
 
 onready var last_position = global_position
 var bounds = Rect2()
+var uv_center = Transform2D(0,Vector2(0.5,0.5))
 
 func _ready():
 	set_notify_transform(true)
@@ -36,7 +37,7 @@ func _notification(what):
 func _process(delta):
 	# look for dead points, searching from the back of the array to avoid issues with removing
 	for i in range(points.size()-1,-1,-1):
-
+		
 		points_data[i][0] += delta
 		if(points_data[i][0] > life):
 			points.remove(i)
@@ -49,14 +50,15 @@ func _process(delta):
 
 	if(points.size() == 0):
 		points.append(global_position)
-		points_data.append([0,0])
+		points_data.append([0.0,0.0])
 
 	var last_point = points[points.size()-1]
 	var square_distance_from_last_point = last_point.distance_squared_to(global_position)
 
-	if(square_distance_from_last_point > maximum_point_distance*maximum_point_distance):
+	if( square_distance_from_last_point > (maximum_point_distance*maximum_point_distance) ):
 		points.append(global_position)
-		points_data.append([0,(last_position-global_position).normalized().angle()])
+		var angle = (last_point - global_position).angle()
+		points_data.append([0,angle])
 
 
 func draw_section(a, a_data, b, b_data):
@@ -67,7 +69,7 @@ func draw_section(a, a_data, b, b_data):
 	var a_transform = Transform2D(a_data[1],a)
 	var b_transform = Transform2D(b_data[1],b)
 
-	var a_color = Color(1,1,1)
+	var a_color = Color(1.0,1.0,1.0)
 	var b_color = a_color
 	if(color):
 		a_color = color.interpolate(a_life)
@@ -76,24 +78,28 @@ func draw_section(a, a_data, b, b_data):
 	var a_width = width
 	var b_width = width
 	if(width_curve):
-		a_width = width_curve.interpolate_baked(1-a_life)*width
-		b_width = width_curve.interpolate_baked(1-b_life)*width
+		a_width = width_curve.interpolate_baked(1.0-a_life)*width
+		b_width = width_curve.interpolate_baked(1.0-b_life)*width
 		
-	var a_v1 = a_transform.xform(Vector2(0,a_width/2))
-	var a_v2 = a_transform.xform(Vector2(0,-a_width/2))
+	var a_width_ratio = 1.0-(a_width/width)
+	var b_width_ratio = 1.0-(b_width/width)
 	
-	var b_v1 = b_transform.xform(Vector2(0,b_width/2))
-	var b_v2 = b_transform.xform(Vector2(0,-b_width/2))
+	var a_final_width = (1.0 - uv_width_factor) * a_width_ratio
+	var b_final_width = (1.0 - uv_width_factor) * b_width_ratio
+		
+	var a_v1 = a_transform.xform(Vector2(0.0,a_width/2))
+	var a_v2 = a_transform.xform(Vector2(0.0,-a_width/2))
 	
-	var w = 1-uv_width_factor
+	var b_v1 = b_transform.xform(Vector2(0.0,b_width/2))
+	var b_v2 = b_transform.xform(Vector2(0.0,-b_width/2))
 	
-	var uv_transform = Transform2D().scaled(uv_scale).translated(Vector2(0.5,0.5)).rotated(deg2rad(uv_rotation)).translated(-Vector2(0.5,0.5))
+	var uv_transform = uv_center * Transform2D(deg2rad(uv_rotation),uv_offset) * uv_center.inverse()
 	
-	var a_uv1 = uv_transform.xform(Vector2(a_life,0.5 * a_life * w))
-	var a_uv2 = uv_transform.xform(Vector2(a_life,0.5 + 0.5 * (1-(a_life * w))))
+	var a_uv1 = uv_transform.xform(Vector2(a_life, a_final_width*0.5))
+	var a_uv2 = uv_transform.xform(Vector2(a_life, 1.0 - a_final_width*0.5))
 	
-	var b_uv1 = uv_transform.xform(Vector2(b_life,0.5 * b_life * w))
-	var b_uv2 = uv_transform.xform(Vector2(b_life,0.5 + 0.5 * (1-(b_life * w))))
+	var b_uv1 = uv_transform.xform(Vector2(b_life, b_final_width*0.5))
+	var b_uv2 = uv_transform.xform(Vector2(b_life, 1.0 - b_final_width*0.5))
 	
 	bounds = bounds.expand(global_transform.affine_inverse().xform(a_v1))
 	bounds = bounds.expand(global_transform.affine_inverse().xform(a_v2))
@@ -135,10 +141,10 @@ func _get_point(i):
 	}
 
 func _draw():
-	bounds.position.x = 0
-	bounds.position.y = 0
-	bounds.size.x = 0
-	bounds.size.y = 0
+	bounds.position.x = 0.0
+	bounds.position.y = 0.0
+	bounds.size.x = 0.0
+	bounds.size.y = 0.0
 	
 	if(points.size() >= 2):
 		
